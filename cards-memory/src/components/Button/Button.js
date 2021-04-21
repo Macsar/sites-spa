a80ad37ff2b1e2d1b8cc2b32305c433d47af930e
Max <limas-m@yandex.ru>
2021-04-21 17:31:39 +1000

    Update

--- cards-memory/src/components/Button/Button.js
+++ cards-memory/src/components/Button/Button.js
@@ -4,12 +4,10 @@ import styles from './Button.module.css';
 // Остальные импорты
 import classnames from 'classnames';
 
-const Button = ({ disabled, click, children }) => {
-    return <button 
-        className = {classnames(styles.btn, disabled ? styles.disabled : '')}
-        onClick = {click}
-        disabled = {disabled}
-    >{ children }</button>
-};
+const Button = ({ disabled, click, children }) => <button 
+	className = {classnames(styles.btn, disabled ? styles.disabled : '')}
+	onClick = {click}
+	disabled = {disabled}
+>{ children }</button>
 
 export default Button;
